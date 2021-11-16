8ece7da1429c3583dbd907019bb7f60325a29e19
edouard3443454 <93147216+edouard3443454@users.noreply.github.com>
2021-11-16 10:23:39 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,37 +2,27 @@ let intensite = 0
 function bleu () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P4, 0)
     pins.digitalWritePin(DigitalPin.P8, 1)
-    pins.digitalWritePin(DigitalPin.P6, 0)
 }
 function blanc () {
-    pins.digitalWritePin(DigitalPin.P6, 1)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P4, 0)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P1, 1)
 }
 function mauve () {
-    pins.digitalWritePin(DigitalPin.P4, 1)
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P8, 1)
-    pins.digitalWritePin(DigitalPin.P6, 0)
 }
 function rouge () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P6, 0)
-    pins.digitalWritePin(DigitalPin.P4, 0)
 }
 function vert () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.digitalWritePin(DigitalPin.P4, 0)
-    pins.digitalWritePin(DigitalPin.P6, 0)
-    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
 }
 basic.forever(function () {
     intensite = pins.analogReadPin(AnalogPin.P0)
