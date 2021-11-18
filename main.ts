392993665a6d81882c9e9d265733bffd6744c1a0
edouard3443454 <93147216+edouard3443454@users.noreply.github.com>
2021-11-18 11:51:35 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -22,21 +22,20 @@ function rouge () {
 function vert () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
 }
 basic.forever(function () {
     intensite = pins.analogReadPin(AnalogPin.P0)
-    if (intensite >= 800) {
+    if (intensite >= 735) {
         vert()
-    } else if (intensite < 800 && intensite >= 600) {
+    } else if (intensite < 735 && intensite >= 600) {
         bleu()
-    } else if (intensite < 600 && intensite >= 400) {
+    } else if (intensite < 600 && intensite >= 550) {
         rouge()
-    } else if (intensite < 0 && false) {
+    } else if (intensite < 550 && intensite >= 550) {
         mauve()
-    } else if (false) {
+    } else if (intensite < 550 && intensite >= 450) {
         blanc()
-    } else {
-    	
     }
+    basic.showNumber(intensite)
 })
